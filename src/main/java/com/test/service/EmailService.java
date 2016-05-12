9d2e142c98884340d72a802d31aa827d53351bd3
prem <chris.prem12@gmail.com>
2016-05-12 11:59:29 +0530

    Fixed Project Setup.

--- src/main/java/com/test/service/EmailService.java
+++ src/main/java/com/test/service/EmailService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Mindfire Solutions
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.test.service;
 
 import java.util.Date;
@@ -12,6 +28,13 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+/**
+ * EmailService class contains methods for email related operations
+ * 
+ * @author mindfire
+ * @version 1.0
+ * @since 10/03/2016
+ */
 @Service
 public class EmailService {
 
@@ -21,31 +44,48 @@ public class EmailService {
 	@Autowired
 	private TemplateEngine templateEngine;
 
-	public void sendSimpleMail(String recepientName, String recpientEmail, Locale locale, String subject,
+	/**
+	 * This method is used to send mails for various operations using java mail.
+	 * 
+	 * @param recipientName
+	 *            the user's name
+	 * @param recipientEmail
+	 *            the user's email
+	 * @param locale
+	 *            the current locale
+	 * @param subject
+	 *            the subject of the mail
+	 * @param confirmationUrl
+	 *            the link to be sent with the mail
+	 * @param template
+	 *            the mail template
+	 * @throws Exception
+	 *             messaging exception
+	 */
+	public void sendSimpleMail(String recipientName, String recipientEmail, Locale locale, String subject,
 			String confirmationUrl, String template) throws Exception {
-		final Context ctx= new Context(locale);
-		
-		ctx.setVariable("name", recepientName);
+		// Prepare the evaluation context
+		final Context ctx = new Context(locale);
+
+		ctx.setVariable("name", recipientName);
 		ctx.setVariable("subscriptionDate", new Date());
 		ctx.setVariable("confirmationUrl", confirmationUrl);
-		
+
+		// Prepare message using a Spring helper
 		final MimeMessage mimeMessage = this.mailSender.createMimeMessage();
 		final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+
 		try {
 			message.setSubject(subject);
 			message.setFrom("bicyclerentaljava@gmail.com");
-			message.setTo(recpientEmail);
-			
-			
-			final String htmlContent =this.templateEngine.process(template, ctx);
-			message.setText(htmlContent,true);
-			
+			message.setTo(recipientEmail);
+
+			// Create the HTML body using Thymeleaf
+			final String htmlContent = this.templateEngine.process(template, ctx);
+			message.setText(htmlContent, true /* isHtml */);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		this.mailSender.send(mimeMessage);
-
-
 	}
-
 }
