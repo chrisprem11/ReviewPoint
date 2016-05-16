5331acb1ae68e89b89d1bf1dae52e9f378fb74b6
prem <chris.prem12@gmail.com>
2016-05-16 17:09:57 +0530

    Added server side pagination for viewing Reviews.

--- src/main/java/com/test/controller/ReviewController.java
+++ src/main/java/com/test/controller/ReviewController.java
@@ -1,8 +1,13 @@
 package com.test.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -27,6 +32,24 @@ public class ReviewController {
 		attributes.addFlashAttribute("success", "Review Submitted Successfully !");
 		
 		return ("redirect:/viewCars/"+reviewDTO.getCarId());
+	}
+	
+	@RequestMapping(value = "reviews/pages/{pageNumber}", method = RequestMethod.GET)
+	public String getReviewPage(@PathVariable Integer pageNumber, Model model) {
+	    Page<Review> page = reviewService.getReviewLog(pageNumber);
+	    List<Review> reviews =page.getContent();
+	    int current = page.getNumber() + 1;
+	    int begin = Math.max(1, current - 5);
+	    int end = Math.min(begin + 10, page.getTotalPages());
+	    int totalPageCount=page.getTotalPages();
+
+	    model.addAttribute("reviews", reviews);
+	    model.addAttribute("beginIndex", begin);
+	    model.addAttribute("endIndex", end);
+	    model.addAttribute("currentIndex", current);
+	    model.addAttribute("totalPageCount",totalPageCount);
+
+	    return "users";
 	}
 
 }
--- src/main/java/com/test/service/ReviewService.java
+++ src/main/java/com/test/service/ReviewService.java
@@ -4,8 +4,12 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.test.DTO.ReviewDTO;
 import com.test.model.Car;
@@ -14,12 +18,15 @@ import com.test.repository.ReviewRepository;
 import com.test.security.CurrentUser;
 
 @Service
+@Transactional
 public class ReviewService {
 
 	@Autowired
 	private CarService carService;
 	@Autowired
 	private ReviewRepository reviewRepository;
+
+	private static final int PAGE_SIZE = 4;
 
 	public Review getReviews(ReviewDTO reviewDTO, Authentication authentication) {
 		Review addReview = new Review();
@@ -34,10 +41,14 @@ public class ReviewService {
 	}
 
 	public List<Review> fetchReviews(Car car, Long carId) {
-		Car cars =carService.getCar(carId);
-		List<Review> allReviews= reviewRepository.findReviewByCar(cars);
+		Car cars = carService.getCar(carId);
+		List<Review> allReviews = reviewRepository.findReviewByCar(cars);
 		return allReviews;
 	}
 
-	
+	public Page<Review> getReviewLog(Integer pageNumber) {
+		PageRequest request = new PageRequest(pageNumber - 1, PAGE_SIZE, Sort.Direction.DESC, "reviewedOn");
+		return reviewRepository.findAll(request);
+	}
+
 }
